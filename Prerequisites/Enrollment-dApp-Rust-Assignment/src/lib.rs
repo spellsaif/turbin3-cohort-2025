67673f8ad2a9f98197df6d5f303dedd07becd291
spellsaif <this.saif.email@gmail.com>
2025-01-09 04:28:57 +0000

    updated: added transfer_sol and transfer_all_sol test

--- Prerequisites/Enrollment-dApp-Rust-Assignment/src/lib.rs
+++ Prerequisites/Enrollment-dApp-Rust-Assignment/src/lib.rs
@@ -1,19 +1,27 @@
 use solana_sdk::{
     signature::{Keypair, Signer, read_keypair_file}, 
     pubkey::Pubkey,
+    message::Message,
+    transaction::Transaction
+
 };
 
 use bs58;
 use std::io::{self, BufRead};
 use solana_client::rpc_client::RpcClient; 
 
+use std::str::FromStr;
+
+use solana_program::{
+    system_instruction::transfer
+};
 
 
 //SOLANA DEVNET URL
 const RPC_URL: &str = "https://api.devnet.solana.com";
 
 //LAMPORTS_PER_SOL
-const LAMPORTS_PER_SOL: u64 = 2_000_000_000; 
+const LAMPORTS_PER_SOL: u64 = 1_000_000_000; 
 
 #[cfg(test)]
 mod tests {
@@ -66,7 +74,7 @@ mod tests {
         // Connected to Solana Devnet RPC Client
         let client = RpcClient::new(RPC_URL); 
 
-        match client.request_airdrop(&keypair.pubkey(), LAMPORTS_PER_SOL) {
+        match client.request_airdrop(&keypair.pubkey(), LAMPORTS_PER_SOL*2) {
 
             Ok(s) => {
                 println!("Success! Check out your TX here:\n");
@@ -82,6 +90,99 @@ mod tests {
     #[test]
     fn transfer_sol() {
         // Test implementation for transfer_sol
+        // Import our keypair
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        
+        // Define our Turbin3 public key
+        let to_pubkey = Pubkey::from_str("AsAduBWNpjJXvW2mN1PXKM1CuHeNYapQQ2VCjZpq9Hbq").unwrap(); 
+        
+        // Create a Solana devnet connection
+        let rpc_client = RpcClient::new(RPC_URL);
+
+        // Get recent blockhash
+        let recent_blockhash = rpc_client
+                                .get_latest_blockhash() 
+                                .expect("Failed to get recent blockhash");
+
+        
+        //Transferring 0.1 sol from dev wallet to turbin3-wallet-address
+        let transaction = Transaction::new_signed_with_payer(
+                            &[transfer(
+                                &keypair.pubkey(), &to_pubkey, LAMPORTS_PER_SOL/10
+                            )],
+                            Some(&keypair.pubkey()), &vec![&keypair], 
+                            recent_blockhash
+                          );
+
+        // Send the transaction
+        let signature = rpc_client
+                        .send_and_confirm_transaction(&transaction)
+                        .expect("Failed to send transaction");
+
+        
+        // Print our transaction out 
+        println!("Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet", signature);
+                            
+    }
+
+
+    #[test]
+    fn transfer_all_sol() {
+        // Test implementation for transfer_sol
+        // Import our keypair
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+
+
+        // Define our Turbin3 public key
+        let to_pubkey = Pubkey::from_str("AsAduBWNpjJXvW2mN1PXKM1CuHeNYapQQ2VCjZpq9Hbq").unwrap(); 
+        
+        // Create a Solana devnet connection
+        let rpc_client = RpcClient::new(RPC_URL);
+
+
+        // Get recent blockhash
+        let recent_blockhash = rpc_client
+                                .get_latest_blockhash() 
+                                .expect("Failed to get recent blockhash");
+
+        
+        // Get balance of dev wallet
+        let balance = rpc_client
+                        .get_balance(&keypair.pubkey())
+                        .expect("Failed to get balance");
+
+
+         //Mock transaction for calculating fees
+         let message = Message::new_with_blockhash(
+                        &[transfer( &keypair.pubkey(), &to_pubkey, balance,)],
+                        Some(&keypair.pubkey()), 
+                        &recent_blockhash
+                       );
+
+        //getting fees
+        let fee = rpc_client
+                    .get_fee_for_message(&message) 
+                    .expect("Failed to get fee calculator");
+        
+        //Transferring 0.1 sol from dev wallet to turbin3-wallet-address
+        let transaction = Transaction::new_signed_with_payer(
+                            &[transfer(
+                                &keypair.pubkey(), &to_pubkey, 
+                                balance-fee
+                            )],
+                            Some(&keypair.pubkey()), &vec![&keypair], 
+                            recent_blockhash
+                          );
+
+        // Send the transaction
+        let signature = rpc_client
+                        .send_and_confirm_transaction(&transaction)
+                        .expect("Failed to send transaction");
+
+        
+        // Print our transaction out 
+        println!("Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet", signature);
+                            
     }
 }
 
